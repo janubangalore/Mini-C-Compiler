cbfc0e286bfb514c1747fe451e0af4f79e081605
Shreeraksha <shreeraksharaithal.181co149@nitk.edu.in>
2020-08-26 20:10:08 +0530

    Added final code and report

---
+++ Part 1 - Lexical Analyser/Code/BasicInput.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main()
+{	
+	/* Hello,
+	This is
+	a sample
+	multi-line 
+	comment*/
+	/* This is a basic program to
+	check the proper execution
+	of Lexical Scanner*/
+	struct node{
+	int a;
+	char name;
+	};
+
+	/*This is a another 
+	multi-line comment*/
+
+	//Following are basic constants and identifiers
+	float fl = 5.01;
+	char l1 = 'a', l2 ='1';
+	int _a=0,b=0,c=5;
+	int arr[50];
+	char *ptr;
+	scanf("%d %d",&a,&b);
+	int sum=0;
+	sum=a+b;
+	printf("\n Sum : %d \n",sum);
+
+	//Enter Name
+	char *name = "Hello";
+	char *name = "John Doe";
+
+	//End the main function
+	return 1;
+}
+void abc()
+{
+	printf("\nTest function ");
+	printf("\nInside abc() function\n");
+}
---
+++ Part 1 - Lexical Analyser/README.md
@@ -0,0 +1,19 @@
+# Lexical Analyser
+
+## Execution
+
+### Windows
+```
+cd Code
+lex LexicalScanner.l
+gcc lex.yy.c
+a.exe <input c-filename>
+```
+
+#### Linux/Ubuntu
+```
+cd Code
+lex LexicalScanner.l
+gcc lex.yy.c
+./a.out <input c-filename>
+```
