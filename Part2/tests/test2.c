da128e9769fa859f264159aa6d342b8e6a92fbdc
janubang <48802804+janubang@users.noreply.github.com>
2020-09-14 13:28:05 +0530

    Update test2.c

--- Part2/tests/test2.c
+++ Part2/tests/test2.c
@@ -1,21 +1,10 @@
 #include<stdio.h>
-
-int fun(char x){
-	return x*x;
-}
-
 void main(){
-	int a=2,b,c,d,e,f,g,h;
-
-	c=a+b;
-	d=a*b;
-	e=a/b;
-	f=a%b;
-	g=a&&b;
-	h=a||b;
-	h=a*(a+b);
-	h=a*a+b*b;
-	h=fun(b);
-
-	//This Test case contains operator,structure,delimeters,Function;
+	int a=2,b=1,c,d;
+	c=a+b;    //Operators
+	d=a%b;
+	c=c&&d;
+	a=a||b;
+	b=a/b;
+	
 }
