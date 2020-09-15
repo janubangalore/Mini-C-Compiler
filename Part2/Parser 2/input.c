d4963d75ba81995e4368ecc47ac52f051996dc71
Shreeraksha <shreeraksharaithal.181co149@nitk.edu.in>
2020-09-16 00:34:15 +0530

    Param List

--- Part2/Parser 2/input.c
+++ Part2/Parser 2/input.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
+void lng(char ma, int t){
+	int y=9;
+}
 int main()
 {
 	int a = 5;
 	int h[5];
 	//Hello
-	char c = 'u';
-	char ii[4+a];
+	char* c = 'u';
+	char* ii[4+a];
 	/* Multi
 	line comment
 	*/
@@ -20,7 +23,7 @@ int main()
 		*/
 		printf("%d",a);
 		a--;
-		int b;
+		int* ii[4+a];
 		while(b>0)
 		{
 			printf("%d", a*b);
---
+++ Part2/Parser 2/y.tab.h
@@ -0,0 +1,111 @@
+/* A Bison parser, made by GNU Bison 2.7.  */
+
+/* Skeleton interface for Bison GLR parsers in C
+   
+      Copyright (C) 2002-2012 Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_Y_TAB_H_INCLUDED
+# define YY_YY_Y_TAB_H_INCLUDED
+/* Enabling traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     NO_ELSE = 258,
+     ELSE = 259,
+     NE_OP = 260,
+     EQ_OP = 261,
+     LE_OP = 262,
+     GE_OP = 263,
+     IDENTIFIER = 264,
+     STRING_CONSTANT = 265,
+     CHAR_CONSTANT = 266,
+     INT_CONSTANT = 267,
+     FLOAT_CONSTANT = 268,
+     SIZEOF = 269,
+     INC_OP = 270,
+     DEC_OP = 271,
+     LEFT_OP = 272,
+     RIGHT_OP = 273,
+     AND_OP = 274,
+     OR_OP = 275,
+     MUL_ASSIGN = 276,
+     DIV_ASSIGN = 277,
+     MOD_ASSIGN = 278,
+     ADD_ASSIGN = 279,
+     SUB_ASSIGN = 280,
+     LEFT_ASSIGN = 281,
+     RIGHT_ASSIGN = 282,
+     AND_ASSIGN = 283,
+     XOR_ASSIGN = 284,
+     OR_ASSIGN = 285,
+     TYPE_NAME = 286,
+     DEF = 287,
+     CHAR = 288,
+     CHAR_ = 289,
+     SHORT = 290,
+     INT = 291,
+     INT_ = 292,
+     LONG = 293,
+     SIGNED = 294,
+     UNSIGNED = 295,
+     FLOAT = 296,
+     DOUBLE = 297,
+     CONST = 298,
+     VOID = 299,
+     IF = 300,
+     WHILE = 301,
+     CONTINUE = 302,
+     BREAK = 303,
+     RETURN = 304,
+     UNARY = 305
+   };
+#endif
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_Y_TAB_H_INCLUDED  */
