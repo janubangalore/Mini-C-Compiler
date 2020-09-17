87bac9cec20c5a51088628ac4aba2bc139d8d86f
Shreeraksha R Aithal <shreeraksharaithal.181co149@nitk.edu.in>
2020-09-17 13:24:05 +0530

    Update Test7.c

--- Part 2 - Syntax Analyzer/Code/Test7.c
+++ Part 2 - Syntax Analyzer/Code/Test7.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
-int main(){
+void main(){
 	/* Example for string quotes not closed*/
 	int a=1;
 	char b[100] = "Help me;
 	int c[10]={0};
+
 }
-// Testcase without a preprocessor directive and proper syntax
