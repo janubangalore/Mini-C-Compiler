1e939cc81f10da274c73b4cb19b4ca890108f7c7
janubang <y.jahnavi56@gmail.com>
2020-11-02 23:50:08 +0530

    Final Changes

---
+++ Part 4 - Intermediate Code Generator/Part 4 - Intermediate Code Generator/tests/test26.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void sum(int a, int b)
+{
+    int c=0;
+    while(a>7)
+    {
+      b=6;
+      while(b>=5)
+      {
+        a=9;
+      }
+    }
+    return;
+}
+
+int main ( )
+{
+  int a=5;
+  int e[5];
+  int f[6];
+  char A[]="Hello";
+	int b=6;
+  while(a>7)
+  {
+    b=6;
+    while(b>=5)
+    {
+      a=9;
+    }
+  }
+	sum(a,b);
+  return 0;
+}
+
