8f4822b9cae5ed5fddc83c9516e49b1c9501221b
Shreeraksha <shreeraksharaithal.181co149@nitk.edu.in>
2020-11-03 17:41:37 +0530

    Added BasicInput.c

---
+++ Part 4 - Intermediate Code Generator/Code/BasicInput.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int number = 0;
+
+int fun(int a, int b, char c)
+{
+  return 2 ;
+}
+
+int main()
+{
+
+  int x = 1;
+
+  int y;
+  int z;
+  int a,b;
+
+  if(x<0)
+  {
+    int y;
+
+    int z=1;
+
+    {
+      int w =1;
+    }
+  }
+  char c='a';
+  for(x=2;x<y;x++)
+  {
+    break;
+  }
+  return 3;
+
+}
